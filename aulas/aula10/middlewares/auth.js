9f03f2794eb3c62c885b58c56b71bd1bbe77c4b7
Gabriel Luna <gabriel.luna.rosseto@gmail.com>
2024-05-24 21:41:32 -0300

    aula11

--- aulas/aula10/middlewares/auth.js
+++ aulas/aula10/middlewares/auth.js
@@ -1,8 +1,17 @@
+const jwt = require("jsonwebtoken");
+
 function validarToken(req, res, next) {
-    if (req.headers.token === "123") {
-        next();
+    const token = req.headers.authorization;
+    if (token) {
+        try {
+            const payload = jwt.verify(token, "123");
+            console.log(payload);
+            next();
+        } catch (e) {
+            res.status(400).json({ msg: "token negado" });
+        }
     } else {
-        res.status(401).json({msg: "n autorizado"})
+        res.status(400).json({ msg: "token invalido" });
     }
 }
 
--- aulas/aula11/controllers/users.js
+++ aulas/aula11/controllers/users.js
@@ -1,17 +1,41 @@
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
 const Usuario = require("../models/users");
 
+function cifrarSenha(senha, salt) {
+    const hash = crypto.createHmac("sha256", salt);
+    hash.update(senha);
+    return hash.digest("hex");
+}
+
 async function criar(req, res) {
-    const novoUsuario = await Usuario.create(req.body);
-    res.status(201).json({ id: novoUsuario._id, email: novoUsuario.email });
+    const salt = crypto.randomBytes(16).toString("hex");
+    const senhaCifrada = cifrarSenha(req.body.senha, salt);
+    const novoUsuario = await Usuario.create({
+        email: req.body.email,
+        senha: senhaCifrada,
+        salt,
+    });
+    res.status(201).json({
+        id: novoUsuario._id,
+        email: novoUsuario.email,
+        senha: novoUsuario.senha,
+        salt: novoUsuario.salt,
+    });
 }
 
 async function entrar(req, res) {
     const usuario = await Usuario.findOne({ email: req.body.email });
     if (usuario) {
-        if (usuario.senha === req.body.senha) res.json({ token: "123" });
+        if (usuario.senha === cifrarSenha(req.body.senha, usuario.salt))
+            res.json({
+                token: jwt.sign({ email: usuario.email }, "123", {
+                    expiresIn: "1m",
+                }),
+            });
         else res.status(401).json({ msg: "acesso negado" });
-    }
-    else res.status(400).json({ msg: "credenciais invalidas" });
+    } else res.status(400).json({ msg: "credenciais invalidas" });
 }
 
 module.exports = { criar, entrar };
