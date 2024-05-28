4213e6d4b6503490507a21e6bf36cc43eaaadb27
Gabriel Luna <gabriel.luna.rosseto@gmail.com>
2024-05-28 20:23:16 -0300

    jwt swagger

--- aulas/aula11/controllers/users.js
+++ aulas/aula11/controllers/users.js
@@ -30,12 +30,35 @@ async function entrar(req, res) {
     if (usuario) {
         if (usuario.senha === cifrarSenha(req.body.senha, usuario.salt))
             res.json({
-                token: jwt.sign({ email: usuario.email }, process.env.JWT_SECRET, {
-                    expiresIn: "1m",
-                }),
+                token: jwt.sign(
+                    { email: usuario.email },
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: "1m",
+                    }
+                ),
             });
         else res.status(401).json({ msg: "acesso negado" });
     } else res.status(400).json({ msg: "credenciais invalidas" });
 }
 
-module.exports = { criar, entrar };
+function renovar(req, res) {
+    const token = req.headers.authorization;
+    if (token) {
+        try {
+            const payload = jwt.verify(token, process.env.JWT_SECRET);
+            res.json({
+                token: jwt.sign(
+                    { email: payload.email },
+                    process.env.JWT_SECRET
+                ),
+            });
+        } catch (e) {
+            res.status(401).json({ msg: "token invalido" });
+        }
+    } else {
+        res.status(401).json({ msg: "token invalido" });
+    }
+}
+
+module.exports = { criar, entrar, renovar };
